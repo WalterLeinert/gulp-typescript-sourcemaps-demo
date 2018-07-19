35811e715676ae516c7ff28625c0efdb436e7d98
Walter Leinert <walter.leinert@aracom.de>
2018-07-19 13:28:32 +0200

    added clean

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,17 @@
 const gulp = require('gulp');
 const ts = require('gulp-typescript');
 const sourcemaps = require('gulp-sourcemaps');
+const rm = require('rimraf');
 
 const project = ts.createProject('./src/tsconfig.json');
 
-gulp.task('default', () => {
+gulp.task('clean', () => {
+	return rm('dist-*', (err) => {
+		console.error(err);
+	});
+});
+
+gulp.task('default', ['clean'], () => {
 	return gulp.src('src/**/*.ts')
 		.pipe(sourcemaps.init())
 		.pipe(ts())
@@ -12,7 +19,7 @@ gulp.task('default', () => {
 		.pipe(gulp.dest("dist-default"));
 });
 
-gulp.task('outdir', () => {
+gulp.task('outdir', ['clean'], () => {
 	return gulp.src('src/**/*.ts')
 		.pipe(sourcemaps.init())
 		.pipe(ts({ outDir: 'dist-outdir' }))
@@ -23,10 +30,10 @@ gulp.task('outdir', () => {
 // Note: The tsconfig.json file has set the `outDir` field, so the setup is almost the same
 // as the `outdir` example. If your tsconfig.json file has no `outDir`, your setup should be
 // similar to the first example.
-gulp.task('tsconfig', () => {
+gulp.task('tsconfig', ['clean'], () => {
 	return project.src()
 		.pipe(sourcemaps.init())
 		.pipe(project())
-		.pipe(sourcemaps.write('.', { sourceRoot: './', includeContent: true }))
+		.pipe(sourcemaps.write('.', { sourceRoot: './', includeContent: false }))
 		.pipe(gulp.dest("dist-tsconfig"));
 });
